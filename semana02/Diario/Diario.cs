c6b2282b46fb70ddb548850477de09ce230f3297
jaamfengland-dot <jaamfengland@gmail.com>
2026-09-11 14:36:43 -0300

    oi

---
+++ semana02/Diario/Diario.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+
+class Diario
+{
+    private List<Registro> _registros;
+
+    public void AdicionarRegistro(Registro novoRegistro)
+    {
+    }
+
+    public void ExibirTodos()
+    {
+    }
+
+    public void SalvarNoArquivo(string arquivo)
+    {
+    }
+
+    public void CarregarDoArquivo(string arquivo)
+    {
+    }
+}
---
+++ semana02/Diario/GeradorDePerguntas.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+class GeradorDePerguntas
+{
+    private List<string> _perguntas;
+
+    public string ObterPerguntaAleatoria()
+    {
+        return "";
+    }
+}
